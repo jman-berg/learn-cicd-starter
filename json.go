d46a1ac825385e127ba44ce33d60357f920c6946
jman-berg <jogchumm@gmail.com>
2025-08-13 21:05:16 +0200

    fixed staticcheck errors

--- json.go
+++ json.go
@@ -30,7 +30,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	if err := w.Write(dat); err != nil {
+	if _, err := w.Write(dat); err != nil {
 		log.Printf("Error writing data in HTTP reply %s", err)
 	}
 }
